bebbfc2407789966c7ae3cfe8350d9628e709f7b
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-08-06 15:53:00 +0100

    Unquote.

--- src/scripts/check.sh
+++ src/scripts/check.sh
@@ -10,8 +10,8 @@ Set_SHELLCHECK_EXCLUDE_PARAM() {
 SC_Main() {
     Set_SHELLCHECK_EXCLUDE_PARAM
     # POSIX
-    find "$SC_PARAM_DIR" -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) \
-        | xargs shellcheck "$SHELLCHECK_EXCLUDE_PARAM" --severity "$SC_PARAM_SEVERITY" | tee -a "$SC_PARAM_OUTPUT"
+    find $SC_PARAM_DIR -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) \
+        | xargs shellcheck $SHELLCHECK_EXCLUDE_PARAM --severity $SC_PARAM_SEVERITY | tee -a "$SC_PARAM_OUTPUT"
 }
 # Will not run if sourced from another script. This is done so this script may be tested.
 # View src/tests for more information.
