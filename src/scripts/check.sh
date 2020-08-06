bd7f0ababfb2adee889646624b5f9ef9b3e90e1d
Gabriel Simmer <gabriel@gitgalaxy.com>
2020-08-06 15:39:28 +0100

    Resolve blank files from find.

--- src/scripts/check.sh
+++ src/scripts/check.sh
@@ -10,12 +10,9 @@ Set_SHELLCHECK_EXCLUDE_PARAM() {
 SC_Main() {
     Set_SHELLCHECK_EXCLUDE_PARAM
     # POSIX
-    find "$SC_PARAM_PATH" -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) \
-        | xargs -0 shellcheck "$SHELLCHECK_EXCLUDE_PARAM" --severity "$SC_PARAM_SEVERITY" "$SC_PARAM_FILES" | tee -a "$SC_PARAM_OUTPUT"
-    ## "$SHELLCHECK_EXCLUDE_PARAM" --severity "$SC_PARAM_SEVERITY" "$SC_PARAM_FILES" | tee -a "$SC_PARAM_OUTPUT"
+    find "$SC_PARAM_DIR" -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) \
+        | xargs shellcheck "$SHELLCHECK_EXCLUDE_PARAM" --severity "$SC_PARAM_SEVERITY" | tee -a "$SC_PARAM_OUTPUT"
 }
 # Will not run if sourced from another script. This is done so this script may be tested.
 # View src/tests for more information.
-if [[ "$_" == "$0" ]]; then
-    SC_Main
-fi
+SC_Main
