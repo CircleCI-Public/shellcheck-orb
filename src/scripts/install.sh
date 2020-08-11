1f9c9bee6d516d78629f883b6c71bf6e6145a35f
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-08-11 16:25:41 -0400

    ignore SC2086 in install script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -12,7 +12,8 @@ if  grep Debian /etc/issue > /dev/null 2>&1 || grep Ubuntu /etc/issue > /dev/nul
     if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we're root
         export SUDO="sudo";
     fi
-    wget -qO- https://github.com/koalaman/shellcheck/releases/download/v$SC_INSTALL_VERSIONshellcheck-v$SC_INSTALL_VERSION.linux.x86_64.tar.xz | tar -xJf -
+    # shellcheck disable=SC2086
+    wget -qO- https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}shellcheck-v${SC_INSTALL_VERSION}.linux.x86_64.tar.xz | tar -xJf -
     cd "shellcheck-v$SC_INSTALL_VERSION/" || false
     $SUDO cp shellcheck /usr/local/bin
     exit $?
@@ -22,7 +23,8 @@ if grep Arch /etc/issue > /dev/null 2>&1; then
     if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we're root
         export SUDO="sudo";
     fi
-    wget -qO- https://github.com/koalaman/shellcheck/releases/download/v$SC_INSTALL_VERSIONshellcheck-v$SC_INSTALL_VERSION.linux.aarch64.tar.xz | tar -xJf -
+    # shellcheck disable=SC2086
+    wget -qO- https://github.com/koalaman/shellcheck/releases/download/v$SC_INSTALL_VERSIONshellcheck-v${SC_INSTALL_VERSION}.linux.aarch64.tar.xz | tar -xJf -
     cd "shellcheck-v$SC_INSTALL_VERSION/" || false
     $SUDO cp shellcheck /usr/local/bin
     exit $?
