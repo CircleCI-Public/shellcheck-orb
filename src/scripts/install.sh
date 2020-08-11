012b1388e28dfd0e747d1351e3278f6aabbdc2ba
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-08-11 16:43:21 -0400

    debug more

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -5,6 +5,7 @@ fi
 
 if grep Alpine /etc/issue > /dev/null 2>&1; then
     apk add shellcheck
+    echo "Alpine"
     exit $?
 fi
 
@@ -24,6 +25,7 @@ if grep Arch /etc/issue > /dev/null 2>&1; then
     if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we're root
         export SUDO="sudo";
     fi
+    echo "DEBUG https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}shellcheck-v${SC_INSTALL_VERSION}.linux.x86_64.tar.xz"
     # shellcheck disable=SC2086
     wget -qO- "https://github.com/koalaman/shellcheck/releases/download/v$SC_INSTALL_VERSIONshellcheck-v${SC_INSTALL_VERSION}.linux.aarch64.tar.xz" | tar -xJf -
     cd "shellcheck-v$SC_INSTALL_VERSION/" || false
