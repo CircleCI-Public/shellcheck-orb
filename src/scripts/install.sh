8b630c67050be7d9457f6072797818239c5493c2
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-08-11 17:24:20 -0400

    manual install on macos

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,5 +1,8 @@
 if echo "$OSTYPE "| grep darwin > /dev/null 2>&1; then
-    brew install "shellcheck@$SC_INSTALL_VERSION"
+    # shellcheck disable=SC2086
+    wget -qO- "https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}/shellcheck-v${SC_INSTALL_VERSION}.darwin.x86_64.tar.xz" | tar -xJf -
+    cd "shellcheck-v$SC_INSTALL_VERSION/" || false
+    $SUDO cp shellcheck /usr/local/bin
     exit $?
 fi
 
@@ -13,7 +16,6 @@ if  grep Debian /etc/issue > /dev/null 2>&1 || grep Ubuntu /etc/issue > /dev/nul
     if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we're root
         export SUDO="sudo";
     fi
-    echo "DEBUG https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}/shellcheck-v${SC_INSTALL_VERSION}.linux.x86_64.tar.xz"
     # shellcheck disable=SC2086
     wget -qO- "https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}/shellcheck-v${SC_INSTALL_VERSION}.linux.x86_64.tar.xz" | tar -xJf -
     cd "shellcheck-v$SC_INSTALL_VERSION/" || false
@@ -25,13 +27,10 @@ if grep Arch /etc/issue > /dev/null 2>&1; then
     if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we're root
         export SUDO="sudo";
     fi
-
-    echo "DEBUG https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}/shellcheck-v${SC_INSTALL_VERSION}.linux.x86_64.tar.xz"
     # shellcheck disable=SC2086
     wget -qO- "https://github.com/koalaman/shellcheck/releases/download/v${SC_INSTALL_VERSION}/shellcheck-v${SC_INSTALL_VERSION}.linux.aarch64.tar.xz" | tar -xJf -
     cd "shellcheck-v$SC_INSTALL_VERSION/" || false
     $SUDO cp shellcheck /usr/local/bin
     exit $?
 fi
-# @TODO: Add Windows support.
-# debug
+
